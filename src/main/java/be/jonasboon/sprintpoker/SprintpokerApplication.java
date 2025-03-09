b70af83fce38a5572f5e703c044071130668511a
Jonas Boon <boon.jonas.git@outlook.com>
2025-03-10 00:42:37 +0100

    Init project

---
+++ src/main/java/be/jonasboon/sprintpoker/SprintpokerApplication.java
@@ -0,0 +1,13 @@
+package be.jonasboon.sprintpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintpokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintpokerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/jonasboon/sprintpoker/SprintpokerApplicationTests.java
@@ -0,0 +1,13 @@
+package be.jonasboon.sprintpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintpokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
